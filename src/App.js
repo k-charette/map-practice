1685dd80e9f1833bcdb8c4afca8bfd2beca7701c
Ken Charette <charettekenneth@gmail.com>
2020-05-25 18:11:26 -0400

    Google maps works. When clicking on map it currently adds a marker to that location

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+  MarkerClusterer
+} from '@react-google-maps/api'
+import { formRelative } from 'date-fns'
+import mapStyles from './mapStyles'
 
-function App() {
+const App = () => {
+
+  const [markers, setMarkers] = useState([])
+
+  const libraries = ["places"]
+  const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh'
+  }
+  const center = { 
+    lat: 35.689487,
+    lng: 139.691711
+  }
+
+  const options = {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true,
+  }
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API,
+    libraries
+  })
+
+
+  if (loadError) return "Error loading maps"
+  if(!isLoaded) return "Loading Maps"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1> Sightings </h1>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={9}
+          center={center}
+          options={options}
+          onClick={(event) => {
+            setMarkers(current => [...current, {
+              lat: event.latLng.lat(),
+              lng: event.latLng.lng(),
+              time: new Date()
+            }])
+          }
+        }
         >
-          Learn React
-        </a>
-      </header>
+
+          {
+            markers.map((marker) => 
+              <Marker 
+                key={marker.time.toISOString()} 
+                position={{lat: marker.lat, lng: marker.lng}} 
+              />)
+          }
+        </GoogleMap>
     </div>
   );
 }
