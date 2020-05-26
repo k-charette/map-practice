0f07890dc703ef85cb87882838299d88eb7d8bd2
Ken Charette <charettekenneth@gmail.com>
2020-05-25 22:31:40 -0400

    Changed marker icon. Opening and closing InfoWindow when clicking marker

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import {
   InfoWindow,
   MarkerClusterer
 } from '@react-google-maps/api'
-import { formRelative } from 'date-fns'
+import { formatRelative } from 'date-fns'
 import mapStyles from './mapStyles'
 
 const App = () => {
 
   const [markers, setMarkers] = useState([])
+  const [selected, setSelected] = useState(null)
 
   const onMapClick = useCallback((event) => {
     setMarkers(current => [...current, {
@@ -52,31 +53,42 @@ const App = () => {
 
   if (loadError) return "Error loading maps"
   if(!isLoaded) return "Loading Maps"
+
   return (
     <div>
       <h1> Sightings </h1>
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
-          zoom={10}
+          zoom={15}
           center={center}
           options={options}
           onClick={onMapClick}
           onLoad={onMapLoad}
         >
-
           {
             markers.map((marker) => 
               <Marker 
                 key={marker.time.toISOString()} 
                 position={{lat: marker.lat, lng: marker.lng}} 
                 icon={{
-                  url: '/forest.svg',
+                  url: '/bunny.svg',
                   scaledSize: new window.google.maps.Size(30,30),
                   origin: new window.google.maps.Point(0, 0),
                   anchor: new window.google.maps.Point(15, 15)
                 }}
+                onClick={() => {
+                  setSelected(marker)
+                }}
               />)
           }
+
+          {selected ? (
+          <InfoWindow position={{ lat: selected.lat, lng: selected.lng }} onCloseClick={() => setSelected(null)}>
+            <div>
+              <h2>Bunny Spotted!</h2>
+              <p>Spotted { formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>) : null}
         </GoogleMap>
     </div>
   );
