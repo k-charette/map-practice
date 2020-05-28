e5ed7eb4508b8b27f966f079485460e6a1a46ede
Ken Charette <charettekenneth@gmail.com>
2020-05-27 20:11:40 -0400

    Styling for the search bar. Will start to autocomplete locations based on your surroundings. Currently only logs the location to the console

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import {
   useLoadScript,
   Marker,
   InfoWindow,
-  MarkerClusterer
 } from '@react-google-maps/api'
 import { formatRelative } from 'date-fns'
+
+import usePlacesAutoComplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from '@reach/combobox'
+import '@reach/combobox/styles.css'
+
 import mapStyles from './mapStyles'
 
 const App = () => {
@@ -50,13 +63,15 @@ const App = () => {
     libraries
   })
 
-
-  if (loadError) return "Error loading maps"
-  if(!isLoaded) return "Loading Maps"
+  if (loadError) return "Error loading map"
+  if(!isLoaded) return "Loading Map"
 
   return (
     <div>
       <h1> Sightings </h1>
+
+      <Search />
+    
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
           zoom={15}
@@ -86,12 +101,54 @@ const App = () => {
           <InfoWindow position={{ lat: selected.lat, lng: selected.lng }} onCloseClick={() => setSelected(null)}>
             <div>
               <h2>Bunny Spotted!</h2>
-              <p>Spotted { formatRelative(selected.time, new Date())}</p>
+              <p>Spotted { formatRelative(selected.time, new Date()) }</p>
             </div>
           </InfoWindow>) : null}
         </GoogleMap>
     </div>
   );
+}
+
+const Search = () => {
+  const { 
+      ready, 
+      value, 
+      suggestions: {status, data}, 
+      setValue, 
+      clearSuggestions
+    } = usePlacesAutoComplete({
+    requestOptions: {
+
+      //TODO For now set to Salem, MA location, change to based on where the user is located
+      location: { lat: () => 42.519539, lng: () => -70.896713 },
+      radius: 200 * 1000,
+    }
+  })
+
+  return (
+    <div className='search'>
+      <Combobox
+        aria-labelledby="search"
+        // receives a prop onSelect that will later receieve the address the user has selected 
+        onSelect={(address) => {console.log(address)}}
+      >
+        <ComboboxInput 
+          style={{width: '400px', height: '40px', fontSize: '24px'}}
+          value={value} 
+          onChange={(e) => {
+            setValue(e.target.value)
+          }}
+          disabled={!ready}
+          placeholder="Enter an address"
+        />
+        <ComboboxPopover>
+          {status === "OK" && data.map(({id, description }) => ( 
+            <ComboboxOption key={id} value={description}/> 
+          ))}
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  )
 }
 
 export default App;
